67d949a49870567a619a17a0522a334b8be8def9
syounglee012 <syounglee012@gmail.com>
2022-03-09 06:04:56 -0800

    1주차 프로젝트 업데이트

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ import urllib.parse
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
+import time
 
 # login setting
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
-
 SECRET_KEY = 'SPARTA'
 
 # mongo db setting
@@ -22,7 +22,18 @@ client = pymongo.MongoClient('mongodb+srv://test:sparta@cluster0.6cz6m.mongodb.n
 db = client.seoul_restroom
 
 # 페이지별 기능 구현
+## 메인페이지 templates
+@app.route('/')
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 
+        return render_template('index.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 @app.route('/login')
 def login():
@@ -68,100 +79,58 @@ def check_dup():
     exists = bool(db.signup.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
-##
-## 구이름 list templates
-# @app.route('/gu_names/<gu_name>')
-# def gu_name(gu_name):
-#    return render_template('{}.html'.format(gu_name))
-
 ### 구별 화장실 list templates
-@app.route('/gu_names/<gu_name>') #태성 주소수정필요
-def rest_list(gu_name):
-    # kangnam all restroom in kangnam
-    # base_info -> if address inccludes "kangman" return it to us
-    # <ul id="restroom_list">
-    #   <a href="/gu_names/강서구/1">우성스포츠센터 1</a>
-    # </ul>
-    #let restrooms_in_gu = list(db.base_info.find({address includes gu_name}, {'_id': False})) # [{name: 'woosung', address: '', id: 1}, {}, {}]
+@app.route('/gu_names/<gu_name>') #태성
+def rest_list(gu_name=None):
+    return render_template('restroom_list.html')
 
-   return render_template('restroom_list.html'.format(gu_name))
-   # return jsonify({ restroom_list: restrooms_in_gu, gu_name: gu_name })
-
-@app.route('/api') #태성
+@app.route('/api/') #태성
 def rest_room_ls():
-   # name, address, star, restroom_id, img_url_1, img_url_2, img_url_3 <== restroom_list
+   # name, address, restroom_id <== restroom_list
    gu_name = request.args.get('guname')
-   # restroom_list = list(db.base_info.find({'adress': gu_name}, {'_id': False}))
-   restroom_list = [{"name" : "test name 1",
-                    "address" : "test address 1",
-                    "star": "test star 1",
-                    "restroom_id": "test restroom_id 1",
-                    "image_url_1": "test image_url_1",
-                    "image_url_2": "test image_url_2",
-                    "image_url_3": "test image_url_3"
-                    },
-                    {"name": "test name 2",
-                     "address": "test address 2",
-                     "star": "test star 2",
-                     "restroom_id": "2222",
-                     "image_url_1": "test image_url_4",
-                     "image_url_2": "test image_url_5",
-                     "image_url_3": "test image_url_6"
-                     },
-                    {"name": "test name 3",
-                     "address": "test address 3",
-                     "star": "test star 3",
-                     "restroom_id": "3333",
-                     "image_url_1": "test image_url_7",
-                     "image_url_2": "test image_url_8",
-                     "image_url_3": "test image_url_9"
-                     }]
+   restroom_list = list(db.base_info.find({ 'address' : { '$regex' : str(gu_name), '$options' : 'i' } }, {'_id': False}))
    return jsonify({"restroom_list" : restroom_list})
 
 ## 성영님
-# mainpage
-@app.route('/gu_names/<gu_name>/<restroom_id>') # 주소수정필요
-def detail_home(gu_name, restroom_id):
+# detail_mainpage
+@app.route('/gu_names/<gu_name>/<rest_room_id>')
+def detail_home(gu_name, rest_room_id):
+    map_id_info = db.district.find_one({"restroom_id" : int(rest_room_id)}, {'_id': False})
+    return render_template('detail.html'.format(gu_name), id=rest_room_id, map_id_info=map_id_info)
+# detail_info_api
+@app.route("/detail_api", methods=["GET"])
+def detail_get():
+    #int 안에 변수들이 NoneType이 들어옴
+    res_Detail = db.base_info.find_one({"restroom_id" : int(request.args.get('id'))},{'_id':False})
+    return jsonify({'details' : res_Detail})
 
-    base_info = db.base_info.find_one({"restroom_id": restroom_id})
-    other_info = db.district.find_one({"restroom_id": base_info},{"_id":False})
-    return render_template('detail.html', x = other_info['X_WGS84'], y = other_info['Y_WGS84'])
-
-
-# detailpage
+# 리뷰를 post할때 화장실 id와 함께 db 저장
 ## review_append
 @app.route("/review", methods=["POST"])
 def reivew_post():
     name_receive = request.form["name_give"]
     comment_receive = request.form["comment_give"]
     star_receive = request.form['star_give']
+    rest_id = request.form["rest_id"]
+
     comment_list = list(db.review.find({},{'_id':False}))
     count = len(comment_list)+1
     doc = {
         'name': name_receive,
         'comment': comment_receive,
         'star': star_receive,
-        'num':count
+        'num':count,
+        "rest_id" : rest_id
     }
+    db.review.insert_one(doc)
+    return jsonify({'msg': '댓글저장 완료!'})
 
-    db.reivew.insert_one(doc)
-    return jsonify({'msg':'댓글저장 완료!'})
-
-## review_show
-@app.route("/reivew", methods=["GET"])
+# 리뷰를 get 할때 화장실 id로 검색하여 클라로 결과전달
+## review_info_api
+@app.route("/review_api", methods=["GET"])
 def review_get():
-    comment_list = list(db.review.find({},{'_id':False}))
+    comment_list = list(db.review.find({"rest_id" : request.args.get("id")},{'_id':False}))
     return jsonify({'comments':comment_list})
-
-
-@app.route("/detail", methods=["GET"])
-def detail_get():
-    #star, name, address <= res_Detail
-    res_Detail = db.detail.find_one({"restroom_id" : request.args.get('gu_names')},{'_id':False})
-    img_list = list(db.img.find({'restroom_id' : request.args.get('gu_names')}))
-    return jsonify({'details' : res_Detail,
-                    'img_list' : img_list})
-
 
 @app.route("/review/delete", methods=["POST"])
 def bucket_undo():
@@ -169,10 +138,8 @@ def bucket_undo():
     db.review.delete_one({'num':int(num_receive)})
     return jsonify({'msg': '삭제완료!'})
 
-
-##
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+   app.run('0.0.0.0',port=5000, debug=True)
 
 
 
---
+++ static/js/ts_js.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    call_restroom_list();
+});
+
+function call_restroom_list() {
+    // {#현재 이 html이 랜딩되고있는 url 추출하기#}
+    var url_string = window.location.href;
+    var url = new URL(url_string);
+    // {#url에서 구이름 추출하기#}
+    var guname = url.href.replace("http://192.168.0.17:5000/gu_names/", "")
+
+    $.ajax({
+        type: 'GET',
+        url: `/api?guname=${guname}`,
+        dataType: 'json',
+        data: {},
+        success: function (response) {
+            let rows = response["restroom_list"]
+            for (i = 0; i < rows.length; i++) {
+                let name = rows[i]["name"]
+                let address = rows[i]["address"]
+                let restroom_id = rows[i]["restroom_id"]
+
+                let current_url = new URL(window.location.href).href
+                let next_url = current_url + "/" + restroom_id
+
+                let temp_html = `<list>
+                                            <a href="${next_url}">
+                                            <article class="message is-info" >
+                                                <div class="message-header">
+                                                   <div class="rest-title"><p>${name}</p></div>
+                                                    
+                                                </div>
+                                                <div class="message-body">
+                                                    <div>
+                                                        <strong>주소 </strong>${address}
+                                                    </div>
+                                                </div>
+                                            </article>
+                                            </a>
+                                        </list>`
+
+                $('#restroom_list').append(temp_html)
+            }
+        }
+    })
+}
